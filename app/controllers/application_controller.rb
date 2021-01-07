8c468c3d6788b7cd03a9be0882d7b9afa0b17b32
sasaki-ichigo <rockthebeat15@docomo.ne.jp>
2021-01-07 12:30:41 +0900

    ストロングパラメーターの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
 
   before_action :basic_auth
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
 
   def basic_auth
@@ -10,6 +12,10 @@ class ApplicationController < ActionController::Base
       # 環境変数を読み込む記述に変更
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
   end
 
 end
