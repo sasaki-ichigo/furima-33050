2c1e4f30164a249ddb336622de091c4e84824771
sasaki-ichigo <rockthebeat15@docomo.ne.jp>
2021-01-09 23:07:01 +0900

    コントローラーを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,14 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+  
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :state_id, :burden_id, :prefecture_id, :day_id, :price ).merge(user_id: current_user.id)
+  end
 
 end
