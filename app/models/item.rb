0d01c929292490e06fa44f662f4bc9e5582a48f0
sasaki-ichigo <rockthebeat15@docomo.ne.jp>
2021-01-09 23:11:29 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
 
+  validates :name, :explanation, :category_id, :state_id, :burden_id, :prefecture_id, :day_id, :price, presence: true
+
   belongs_to :user
   # has_one :order
 
