3cdb5ea81b94075cd1245bf783d927b2329ec7e4
sasaki-ichigo <rockthebeat15@docomo.ne.jp>
2021-01-18 12:26:13 +0900

    Formオブジェクトを用いた購入情報の登録機能の実装2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -53,6 +53,15 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless current_user.id == @item.user_id
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    elsif 
+      @item.order != nil
+      redirect_to root_path
+    end
   end
 end
+
+# def move_to_index
+#   redirect_to action: :index unless current_user.id == @item.user_id
+# end
